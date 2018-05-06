04799670aa9704ef2c72c2eb6110ed62a309233f
Fred Uggla <Fred.Uggla@gmail.com>
2018-05-07 00:29:41 +0200

    Calculate and print distance to destination from current position

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local version = 0.1
+require("util")
 
 local navigation = {
 	position = {
@@ -14,8 +15,20 @@ local navigation = {
 	}
 }
 
+function love.load()
+	navigation.destination = {
+		x = 10,
+		y = 10,
+		z = 10
+	}
 end
 
 function love.draw()
 	love.graphics.printf("Lampyrid v" .. version, 5, 5, 100)
+	love.graphics.printf("Distance: " .. navigation.destination.distance, 5, 20, 100)
+end
+
+function love.update()
+	navigation.destination.distance = math.dist(navigation.position.x, navigation.position.y, navigation.position.z, navigation.destination.x, navigation.destination.y, navigation.destination.z)
+	navigation.destination.distance = math.round(navigation.destination.distance)
 end
