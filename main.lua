58a8d24c7909158d58a0103f73ae901eba6bfa0a
Fred Uggla <Fred.Uggla@gmail.com>
2018-05-07 18:39:58 +0200

    Shortened navigation table names

--- main.lua
+++ main.lua
@@ -1,22 +1,21 @@
 local version = 0.1
 require("util")
 
-local navigation = {
-	position = {
-		x = 0,
-		y = 0,
-		z = 0
-	},
-	destination = {
-		x = 0,
-		y = 0,
-		z = 0,
-		distance = 0
-	}
+position = {
+	x = 0,
+	y = 0,
+	z = 0
+}
+
+destination = {
+	x = 0,
+	y = 0,
+	z = 0,
+	distance = 0
 }
 
 function love.load()
-	navigation.destination = {
+	destination = {
 		x = 10,
 		y = 10,
 		z = 10
@@ -25,10 +24,10 @@ end
 
 function love.draw()
 	love.graphics.printf("Lampyrid v" .. version, 5, 5, 100)
-	love.graphics.printf("Distance: " .. navigation.destination.distance, 5, 20, 100)
+	love.graphics.printf("Distance: " .. destination.distance, 5, 20, 100)
 end
 
 function love.update()
-	navigation.destination.distance = math.dist(navigation.position.x, navigation.position.y, navigation.position.z, navigation.destination.x, navigation.destination.y, navigation.destination.z)
-	navigation.destination.distance = math.round(navigation.destination.distance)
+	destination.distance = math.dist(position.x, position.y, position.z, destination.x, destination.y, destination.z)
+	destination.distance = math.round(destination.distance)
 end
