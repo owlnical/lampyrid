9fabcaeb3d2cb8f0c7bc2f29d6fd3d7ee6a4ec9d
Fred Uggla <Fred.Uggla@gmail.com>
2018-05-15 01:00:01 +0200

    Added several methods for working with the full history string

--- class/terminal.lua
+++ class/terminal.lua
@@ -5,28 +5,48 @@ local Terminal = class("Terminal")
 
 
 function Terminal:initialize(text, prefix, suffix)
-	self.command = ""					-- The current (not yet executed) command
-	self.current = 1 					-- The currently viewed command
-	self.history = {{edited = "", original = ""}}					-- History of command
-	self.text = text or ""				-- All previous input/output as a single string
+	self.history = text or ""				-- All previous input/output as a single string
 	self.prefix = prefix or "$ "		-- Characters beforce the command
 	self.suffix = suffix or "█"			-- Characters after the command
+
+
+	--[[
+		Store current and previous commands in a single table
+		edited:		The current string
+		original:	String when executed
+	--]]
+	self.command = {{edited = "", original = ""}}
+
+	-- The currently viewed command
+	self.current = 1
 end
 
 function Terminal:getCommand()
-	return self.history[self.current].edited
+	return self.command[self.current].edited
 end
 
 function Terminal:setCommand(text)
-	self.history[self.current].edited = text
+	self.command[self.current].edited = text
 end
 
 function Terminal:appendCommand(text)
 	self:setCommand(self:getCommand() .. text)
 end
 
-function Terminal:input(text)
-	self:setCommand(self:getCommand() .. text)
+function Terminal:setHistory(text)
+	self.history = text
+end
+
+function Terminal:getHistory(text)
+	return self.history
+end
+
+function Terminal:appendHistory(text)
+	self.history = self.history .. text
+end
+
+function Terminal:getContent()
+	return self.history .. self.prefix .. terminal:getCommand() .. self.suffix
 end
 
 -- Erase UTF-8 characters (https://love2d.org/wiki/love.textinput)
@@ -41,41 +61,56 @@ end
 function Terminal:move(direction)
 	if direction == "up" and self.current > 1 then
 			self.current = self.current - 1
-	elseif direction == "down" and self.current < #self.history then
+	elseif direction == "down" and self.current < #self.command then
 			self.current = self.current + 1
 	end
 end
 
 -- Print text to Terminal
 function Terminal:print(text)
-	self.text = self.text .. text
+	self.history = self.history .. text
 end
 
 -- Split input string into command and args
 function Terminal:split(input)
 	local args = string.split(input or self:getCommand())
 	local command = args[1]
-	self.text = self.text .. self.prefix .. self:getCommand() .. "\n"
 	table.remove(args, 1)
 	return command, args
+end
+
+-- Add the current command to the history
+function Terminal:updateHistory()
+	self.command[#self.command].original = self:getCommand()
+	self.command[#self.command].edited = self:getCommand()
+
+	--[[
+		If this was a partially edited previous command
+		Restore it to it's original string.
+	--]]
+	if self.current < #self.command then
+		self.command[self.current].edited = self.command[self.current].original
+	end
+end
+
+-- Add a new command to the bottom of the history table
+function Terminal:newCommand()
+	self.command[#self.command + 1] = {edited = "", original = ""}
+	self.current = #self.command
 end
 
 -- Run current input
 function Terminal:run()
 	local command, arg = self:split()
+	self:appendHistory(self.prefix .. self:getCommand() .. "\n")
 	if command ~= "" then
 		if false then
 			-- find commands
 		else
 			self:print("Command not found\n")
 		end
-		self.history[#self.history].original = self:getCommand() 	-- Store the current command as original
-		self.history[#self.history].edited = self:getCommand()		-- Store the current command as the edit
-		self.history[self.current].edited = self.history[self.current].original -- If we edited an old command. Restore it to original
-
-		-- Start next command
-		self.history[#self.history + 1] = {edited = "", original = ""}
-		self.current = #self.history
+		self:updateHistory()
+		self:newCommand()
 	end
 end
 
--- main.lua
+++ main.lua
@@ -43,7 +43,7 @@ function love.draw()
 		love.graphics.setColor(0.1, 0.1, 0.1, 1)
 		love.graphics.rectangle("fill", 000,000, love.graphics.getDimensions())
 		love.graphics.setColor(1, 1, 1)
-	  love.graphics.printf(terminal.text .. terminal.prefix .. terminal:getCommand() .. terminal.suffix, 20, 20, love.graphics.getWidth()-50)
+	  love.graphics.printf(terminal:getContent(), 20, 20, love.graphics.getWidth()-50)
     end)
 end
 
@@ -71,7 +71,7 @@ end
 
 -- Add input to the terminal
 function love.textinput(text)
-	terminal:input(text)
+	terminal:appendCommand(text)
 end
 
 function love.keypressed(key)
