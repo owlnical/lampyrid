c44a90735d0900d9ab8a9de7b4360613afe44b25
Fred Uggla <fred.uggla@owlnical.net>
2019-05-29 13:02:04 +0200

    pos & dest vec3 -> table
    The vec3 data type can't be sent over a channel

--- data.lua
+++ data.lua
@@ -3,8 +3,8 @@ local cpml = require("lib/cpml")
 local channel = love.thread.getChannel("data")
 data = {
 	-- Navigation
-	position = cpml.vec3.new(0, 0, 0),
-	destination = cpml.vec3.new(0, 0, 0),
+	position = {0, 0, 0},
+	destination = {0, 0, 0},
 	distance = 0,
 	travel_time = 0,
 	speed = 10,
