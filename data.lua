3aeeeea69f6fa1280de6e6fdb46aa207a88c2143
Fred Uggla <fred.uggla@owlnical.net>
2019-05-29 18:45:14 +0200

    refactored data.travel()

--- data.lua
+++ data.lua
@@ -5,14 +5,14 @@ data = {
   -- Navigation
   position = {0, 0, 0},
   destination = {25, 25, 25},
-  position3 = cpml.vec3.new(0, 0, 0),
-  destination3 = cpml.vec3.new(25, 25, 25),
   distance = 0,
   travel_time = 0,
   speed = 5,
   traveling = true,
   eta = 0
 }
+
+vec3 = {}
 
 function data.get(name)
   local reply = false
@@ -34,34 +34,37 @@ end
 -- Move forward by calculating lerp step from position, distance, speed and deltatime
 function data.travel(dt)
 
+  -- Convert the destination/position tables to vectors
+  vec3.destination = cpml.vec3.new(data.destination)
+  vec3.position = cpml.vec3.new(data.position)
+
   -- Current distance to destination
-	data.distance = data.position3:dist(data.destination3)
+	data.distance = vec3.position:dist(vec3.destination)
 
   -- How far we will move
+  -- How I came up with this black magic last year is now beyond me
+  -- But it seems to be working
 	local step = (data.distance / (data.distance - (dt * data.speed))) - 1
 
   -- We're not here yet
+  -- Move towards the destination from the position based on the step
 	if step < 1 then
-
-    -- Move towards the destination from the position based on the step
-		data.position3 = cpml.vec3.lerp(data.position3, data.destination3, step)
+		vec3.position = cpml.vec3.lerp(vec3.position, vec3.destination, step)
 		data.travel_time = data.travel_time + dt
 		data.eta = cpml.utils.round(data.distance / data.speed / 60)
 
-    --We're here
+    --We're here, reset all traveling values
 	else
 		data.traveling = false
 		data.travel_time = 0
 		data.distance = 0
 		data.eta = 0
-		data.position3 = data.destination3:clone()
+		vec3.position = vec3.destination:clone()
 	end
 
   -- Update the position and destination tables with the current vector
-  local x, y, z = cpml.vec3.unpack(data.destination3)
-  data.destination = {x, y, z}
-  x, y, z = cpml.vec3.unpack(data.position3)
-  data.position = {x, y, z}
+  data.destination = {cpml.vec3.unpack(vec3.destination)}
+  data.position = {cpml.vec3.unpack(vec3.position)}
 end
 
 -- Main loop listening for commands
