35dd638a7809cb58a1fa908713e945cf43f4de9f
Fred Uggla <fred.uggla@owlnical.net>
2019-05-30 17:19:22 +0200

    Start traveling when the destinatin has changed

--- programs/destination.lua
+++ programs/destination.lua
@@ -31,8 +31,15 @@ function destination.set(x, y, z)
   y = tonumber(y)
   z = tonumber(z)
   if x and y and z then
+    -- Here we go!
     set("destination", {x, y, z})
+    set("traveling", true)
     write(string.format("Destination set to %s.%s.%s", x, y, z))
+
+    -- Trigger calculation and output data
+    channel.data:supply({"travel", 0.01})
+    write(string.format("Distance to destination: %s", get("distance")))
+    write(string.format("ETA: %s minutes", get("eta")))
   else
     write("Unable to set destination. Try destination help")
   end
