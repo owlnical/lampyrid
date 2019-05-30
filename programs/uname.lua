3da75aa2f4b693e1a9dc4fe5a0dcc027070904ce
Fred Uggla <fred.uggla@owlnical.net>
2019-05-30 15:04:35 +0200

    Added help and functions to uname

--- programs/uname.lua
+++ programs/uname.lua
@@ -1,2 +1,22 @@
 require "channel"
-write(string.format("Love %s.%s.%s %s", love.getVersion()))
+local uname = require("program")
+name = "uname"
+help = [[
+uname (Lampyrid core) 1.0
+
+This program prints the ships OS kernel.
+
+  Usage: uname
+
+  get                 Print kernel
+  show                Alias: get
+  help                Show this help
+]]
+
+function uname.get()
+  write(string.format("Love %s.%s.%s %s", love.getVersion()))
+end
+
+uname.show = uname.get
+
+uname.__run()
