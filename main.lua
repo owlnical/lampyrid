29b2924d359aa2d52a8947abd6b4c13c4e7bfc9e
Fred Uggla <fred.uggla@owlnical.net>
2020-04-02 22:29:50 +0200

    moonshine shader emulating crt monitor

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local version = 0.2
 local cargo = require('lib/cargo')
+local moonshine = require("lib/moonshine")
 local class = require "lib/middleclass"
 local Terminal = require("class/terminal")
 
@@ -17,10 +18,26 @@ function love.load()
 	-- Main terminal
 	terminal = Terminal:new("Main Terminal")
 	terminal:appendBuffer("\nWelcome to Lampyrid v" .. version .. "\n")
+
+	-- Shaders to emulate a crt monitor
+	shader = moonshine(moonshine.effects.crt)
+		.chain(moonshine.effects.scanlines)
+		.chain(moonshine.effects.vignette)
+		.chain(moonshine.effects.glow)
+	shader.parameters = {
+		crt = {distortionFactor = {1.06, 1.065}, x = 10, y = 50},
+		scanlines = {opacity = 0.1},
+		vignette = {opacity = 0.1}
+	}
 end
 
 function love.draw()
-	terminal:draw()
+	shader(function()
+		-- Monitor glass and terminal text
+		love.graphics.setColor(0.1, 0.1, 0.1, alpha)
+		love.graphics.rectangle("fill", 0, 0, love.graphics.getDimensions())
+		terminal:draw()
+	end)
 end
 
 function love.update(dt)
