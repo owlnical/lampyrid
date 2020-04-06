4bcea3726d23fab77d631ab0f9df5fea27618fe1
Fred Uggla <fred.uggla@owlnical.net>
2020-04-07 00:04:53 +0200

    removed particle and image assets

--- assets/particles/draw.lua
+++
@@ -1,6 +0,0 @@
-function drawparticles(name)
-  love.graphics.draw(particles[name] )
-  love.graphics.setColor(0.1, 0.1, 0.1, 0.3)
-  love.graphics.rectangle("fill", 000,000, love.graphics.getDimensions())
-end
-return drawparticles
