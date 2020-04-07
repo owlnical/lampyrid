5cc23e104740c19ddcaf8da081c8c5b6dad66fa5
Fred Uggla <fred.uggla@owlnical.net>
2020-04-07 21:46:09 +0200

    system listens to reqs on the request channel

--- system.lua
+++ system.lua
@@ -1,11 +1,23 @@
-delta = love.thread.getChannel("delta")
-system = love.thread.getChannel("system")
+system = {
+	request = love.thread.getChannel("request"),
+	delta = love.thread.getChannel("delta")
+}
 
 function main()
-	local ram = load()
+	ram = load()
 	local dt = 0
+	local request
+
 	while true do
-		dt = delta:demand()
+		dt = system.delta:demand()
+		while system.request:getCount() > 0 do
+			request = system.request:pop()
+			if system[request[1]] then
+				system[request[1]](request)
+			else
+				system:supply(false)
+			end
+		end
 	end
 end
 
