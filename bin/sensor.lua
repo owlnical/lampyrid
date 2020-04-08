f07ba515a9f9fcc889676c4d0c52113af1be2269
Fred Uggla <fred.uggla@owlnical.net>
2020-04-08 23:36:29 +0200

    sensor can set destination

--- bin/sensor.lua
+++ bin/sensor.lua
@@ -16,8 +16,22 @@ function sensor.position()
 	print("Position:", sys.uget("nav", "position"))
 end
 
-function sensor.destination()
-	print("Destination:", sys.uget("nav", "destination"))
+function sensor.destination(args)
+	local cmd, arg = sys.to_cmd_args(args)
+
+	if cmd == "set" then
+		local x = tonumber(arg[1])
+		local y = tonumber(arg[2])
+		local z = tonumber(arg[3])
+		if x and y and z then
+			sys.set("nav", "destination", arg)
+			print("Destination changed to", x, y, z)
+		else
+			print("Error: invalid destination:", x, y, z)
+		end
+	elseif args[1] == nil then
+		print("Destination:", sys.uget("nav", "destination"))
+	end
 end
 
 -- short
