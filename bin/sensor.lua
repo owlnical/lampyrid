a94bcd5780551fea89ef289f9cb91b5303e321db
Fred Uggla <fred.uggla@owlnical.net>
2020-04-08 15:57:19 +0200

    renamed position program to sensor

--- bin/sensor.lua
+++ bin/sensor.lua
@@ -1,7 +1,18 @@
 system = love.thread.getChannel("system")
+args = love.thread.getChannel("args")
 output = love.thread.getChannel("output")
+local sensor = {}
 
 function main()
+	local a = args:demand()
+	if a[1] and sensor[a[1]] then
+		sensor[a[1]](a)
+	else
+		output:push("sensor: unknown argument\n")
+	end
+end
+
+function sensor.position()
 	system:supply({"get", "nav", "position"})
 	output:push(string.format(
 		"position %s.%s.%s\n",
