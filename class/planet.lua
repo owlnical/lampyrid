99596d482ba790eabf1e028cc482c56c90db1a8e
Fred Uggla <fred.uggla@owlnical.net>
2020-04-21 21:16:33 +0200

    added random generator to Planet

--- class/planet.lua
+++ class/planet.lua
@@ -6,9 +6,9 @@ function Planet:initialize(seed, name)
 	self.rng = love.math.newRandomGenerator(seed)
 
 	-- random name. Might have prefix and suffix.
-	local p = name.prefix[self.rng:random(1, #name.prefix*3)] or ""
-	local m = name.main[self.rng:random(1, #name.main)]
-	local s = name.suffix[self.rng:random(1, #name.suffix*3)] or ""
+	local p = name.prefix[self:random(1, #name.prefix*3)] or ""
+	local m = name.main[self:random(1, #name.main)]
+	local s = name.suffix[self:random(1, #name.suffix*3)] or ""
 	self.name = string.format("%s %s %s", p, m, s):gsub("^%s",""):gsub("%s$","")
 end
 
@@ -19,5 +19,14 @@ end
 function Planet:getName()
 	return self.name
 end
+
+function Planet:random(l, h)
+	if l and h then
+		return self.rng:random(l*100, h*100)/100
+	else
+		return self.rng:random()
+	end
+end
+
 
 return Planet
